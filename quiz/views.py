94d0f8d8312db099134edab7da0e63ab0c151e26
FreddieStern <31613156+FreddieStern@users.noreply.github.com>
2017-09-28 11:48:56 +0200

    #4 Extracted data from data base

--- quiz/views.py
+++ quiz/views.py
@@ -1,25 +1,26 @@
+from quiz.models import Quiz
 from django.shortcuts import render
-quizzes = [
-	{
-		"quiz_number": 1,
-		"name": "Klassiska böcker",
-		"description": "Hur bra kan du dina klassiker?"
-	},
-	{
-		"quiz_number": 2,
-		"name": "Största fotbollslagen",
-		"description": "Kan du dina lag?"
-	},
-	{
-		"quiz_number": 3,
-		"name": "Världens mest kända hackare",
-		"description": "Kan du din hackerhistoria?"
-	},
-]
+# quizzes = [
+# 	{
+# 		"quiz_number": 1,
+# 		"name": "Klassiska böcker",
+# 		"description": "Hur bra kan du dina klassiker?"
+# 	},
+# 	{
+# 		"quiz_number": 2,
+# 		"name": "Största fotbollslagen",
+# 		"description": "Kan du dina lag?"
+# 	},
+# 	{
+# 		"quiz_number": 3,
+# 		"name": "Världens mest kända hackare",
+# 		"description": "Kan du din hackerhistoria?"
+# 	},
+# ]
 
 def startpage (request):
 	context = {
-			"quizzes": quizzes,
+			"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "startpage.html", context)
 
