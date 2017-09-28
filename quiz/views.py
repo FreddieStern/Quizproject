98af5171d9487cfc4692d9d5dca73719f1d1cfdf
FreddieStern <31613156+FreddieStern@users.noreply.github.com>
2017-09-28 14:53:48 +0200

    #5 Added radio buttons

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from quiz.models import Quiz
 from django.shortcuts import render
 # quizzes = [
@@ -32,13 +33,17 @@ def quiz(request, quiz_number):
 	return render(request,"quiz.html", context)
 
 def question(request, quiz_number, question_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	question = questions[question_number - 1]
 	context = {
-			"question_number": question_number,
-			"question": "Hur många bultar har ölandsbron?",
-			"answer1": "12",
-			"answer2": "66 400",
-			"answer3": "7 428 954",
-			"quiz_number": quiz_number,
+		"question_number": question_number,
+		"question": question.question,
+		"answer1": question.answer1,
+		"answer2": question.answer2,
+		"answer3": question.answer3,
+		"quiz": quiz,
+		"quiz_number": quiz_number,
 	}
 	return render(request, "question.html", context)
 
@@ -49,5 +54,13 @@ def completed(request, quiz_number):
 			"quiz_number": quiz_number,
 	}
 	return render(request, "completed.html", context)
+
+def answer(request, quiz_number, question_number):
+	answer = request.POST["answer"]
+	saved_answers = request.session.get(str(quiz_number), {})
+	saved_answers[question_number] = int(answer)
+	request.session[quiz_number] = saved_answers
+	return redirect("question_page", quiz_number, question_number + 1)
+	
 
 # Create your views here.
